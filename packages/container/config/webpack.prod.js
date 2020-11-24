8189a466ff62cb110a4178d644cf18f47787d098
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-11-24 15:02:00 -0600

    build test

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
@@ -8,7 +9,6 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
@@ -16,6 +16,7 @@ const prodConfig = {
       remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
+      shared: packageJson.dependencies,
     }),
   ],
 };
