de5fd38b87a217b851f9392cc37d0c0e395141d9
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-11-24 10:08:52 -0600

    initial commit

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,21 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": `./src/bootstrap`,
+      },
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
