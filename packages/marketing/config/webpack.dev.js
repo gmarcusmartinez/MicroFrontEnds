56ac1578ff7ced07ed0c32ddadcb1e70dc915bc4
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-11-24 15:39:53 -0600

    final test

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,28 +1,28 @@
-const { merge } = require("webpack-merge");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const packageJson = require("../package.json");
-const commonConfig = require("./webpack.common");
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 8081,
     historyApiFallback: {
-      index: "index.html",
+      index: 'index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "marketing",
-      filename: "remoteEntry.js",
+      name: 'marketing',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./MarketingApp": "./src/bootstrap",
+        './MarketingApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
-    new HTMLWebpackPlugin({
-      template: "./public/index.html",
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
     }),
   ],
 };
