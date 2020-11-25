cff61e84424ce12341bc7a7e809f522d82d5cad6
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-11-25 08:25:19 -0600

    bug in import statement

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-domÎ";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
